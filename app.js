6452a7459e0550b5b2ebf9c1133e846c6e6dbd27
Ariana Belgrado <belgradoariana@gmail.com>
2022-09-06 20:45:41 -0300

    agregue lo de instagram

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+
+let app = express();
+
+let path= require('path');
+
+app.listen(3000, () => 
+console.log('servidor corriendo')
+);
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname,'/public/home.html'))
+})
