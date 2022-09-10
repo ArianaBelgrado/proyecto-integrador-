3c1fe37d87bc169faf790b13250a192b918272ca
Ariana Belgrado <belgradoariana@gmail.com>
2022-09-10 12:12:35 -0300

    agregue el log in e sign in.html

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ let app = express();
 
 let path= require('path');
 
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
 app.listen(3000, () => 
 console.log('servidor corriendo')
 );
 
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname,'/views/log-in.html'))
+})
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname,'/views/sign-in.html'))
 })
