4fc75f3d456577d2a78c2f7c419f742c6469365a
Ariana Belgrado <belgradoariana@gmail.com>
2022-09-20 20:54:16 -0300

    comenzando a subir

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let express = require('express');
 
 let app = express();
@@ -16,35 +15,10 @@ app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 
-app.get('/', function(req,res) {
+app.get('/log-in', function(req,res) {
     res.sendFile(path.join(__dirname,'/views/log-in.html'))
 })
 
-app.get('/', function(req,res) {
+app.get('/sign-in', function(req,res) {
     res.sendFile(path.join(__dirname,'/views/sign-in.html'))
-=======
-let express = require('express');
-
-let app = express();
-
-let path= require('path');
-
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
-
-app.listen(3000, () => 
-console.log('servidor corriendo')
-);
-
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/home.html'))
-})
-
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/log-in.html'))
-})
-
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/sign-in.html'))
->>>>>>> 79db696965756f3a6cd5b321ebd37ead4d9d3160
 })
