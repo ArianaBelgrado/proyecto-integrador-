b65db4b7edf4bf6fd6e47aec09e514e9f9b71a4d
Juanzor <hjuan7217@gmail.com>
2022-09-22 01:08:18 -0300

    Heroku

--- app.js
+++ app.js
@@ -2,32 +2,36 @@ let express = require('express');
 
 let app = express();
 
-let path= require('path');
+let path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => 
-console.log('servidor corriendo')
+app.listen(3000, () =>
+    console.log('servidor corriendo')
 );
 
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Servidor corriendo");
 })
 
-app.get('/log-in', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/log-in.html'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
-app.get('/sign-in', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/sign-in.html'))
+app.get('/log-in', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/log-in.html'))
+})
+
+app.get('/sign-in', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/sign-in.html'))
 })
 
 
- app.get('/detalle', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/detalle.html'))
- })
+app.get('/detalle', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/detalle.html'))
+})
 
- app.get('/carrito', function(req,res) {
-    res.sendFile(path.join(__dirname,'/views/carrito.html'))
- })
+app.get('/carrito', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'))
+})
